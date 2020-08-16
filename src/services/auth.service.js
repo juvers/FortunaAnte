a05d102c288834b3f9326641509135717c2f117c
Jude kuti <judekuti@gmail.com>
2020-08-16 10:30:13 -0500

    Write login service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -11,3 +11,18 @@ const register = (username, email, password) => {
         password,
     });
 };
+
+const login = (username, password) => {
+    return axios
+        .post(API_URL + "signin", {
+            username,
+            password,
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
