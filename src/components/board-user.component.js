0fc8d205bf4ee90f0305103999478d4518baa277
Jude kuti <judekuti@gmail.com>
2020-08-16 14:33:02 -0500

    Use images from my s3 bucket

--- src/components/board-user.component.js
+++ src/components/board-user.component.js
@@ -37,6 +37,154 @@ export default class BoardUser extends Component {
         <header className="jumbotron">
           <h3>{this.state.content}</h3>
         </header>
+        <div className="grid">
+            <div className="item">
+                <div className="item__details">
+                    jelly-o brownie sweet
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    Muffin jelly gingerbread
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    sesame snaps chocolate
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    Oat cake
+                </div>
+            </div>
+            <div className="item item--full">
+                <div className="item__details">
+                    jujubes cheesecake
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    Dragée pudding brownie
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    Oat cake
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    powder toffee
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    pudding cheesecake
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    toffee bear claw
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    cake cookie croissant
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    liquorice sweet roll
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    chocolate marzipan
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    danish dessert lollipop
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    sugar plum dragée
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    powder toffee
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    pudding cheesecake
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    toffee bear claw
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    cake cookie croissant
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    liquorice sweet roll
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    chocolate marzipan
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    danish dessert lollipop
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    sugar plum dragée
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    danish dessert lollipop
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    sugar plum dragée
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    powder toffee
+                </div>
+            </div>
+            <div className="item item--medium">
+                <div className="item__details">
+                    pudding cheesecake
+                </div>
+            </div>
+            <div className="item item--large">
+                <div className="item__details">
+                    toffee bear claw
+                </div>
+            </div>
+            <div className="item">
+                <div className="item__details">
+                    cake cookie croissant
+                </div>
+            </div>
+        </div>
+
       </div>
     );
   }
